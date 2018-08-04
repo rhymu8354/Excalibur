c0a2954b890e0508179b7885c700d5dced30f6df
Richard Walters <rwalters@digitalstirling.com>
2018-08-04 13:21:09 -0700

    Chatty: add list of people in the chat room

--- chatter/src/App.js
+++ chatter/src/App.js
@@ -21,7 +21,13 @@ class App extends Component {
             /**
              * This is the nickname that is being set in the server.
              */
-            nicknameBeingSet: '',
+            nicknameToSet: '',
+
+            /**
+             * These are the nicknames of the users currently
+             * in the chat room.
+             */
+            nicknames: [],
         };
         this.socket = null;
     }
@@ -54,30 +60,60 @@ class App extends Component {
         this.setState({
             socketStatus: "CONNECTED",
         });
+        if (this.state.newNickname !== '') {
+            this.onSetNickname();
+        }
+        this.socket.send(JSON.stringify({Type: "GetNickNames"}));
     }
 
     onMessageReceived = (message) => {
-        switch(message.Type) {
-            case 'SetNickNameResult': {
+        switch (message.Type) {
+            case 'SetNickNameResult':
                 if (message.Success) {
                     this.setState({
-                        currentNickname: this.state.nicknameBeingSet,
+                        currentNickname: this.state.nicknameToSet,
+                        nicknames: [...this.state.nicknames, this.state.nicknameToSet],
                     });
                 }
-            } break;
+                break;
+
+            case 'NickNames':
+                this.setState({
+                    nicknames: message.NickNames,
+                });
+                break;
+
+            case 'Join':
+                this.setState({
+                    nicknames: [...this.state.nicknames, message.NickName],
+                });
+                break;
+
+            case 'Leave':
+                this.setState({
+                    nicknames: this.state.nicknames.filter(
+                        nickname => nickname !== message.NickName
+                    ),
+                });
+                break;
+
+            default:
+                break;
         }
     }
 
     onDisconnect = () => {
         this.setState({
             socketStatus: "disconnected",
+            nicknames: [],
+            currentNickname: '',
         });
         this.socket.close();
     }
 
     onSetNickname = () => {
         this.setState({
-            nicknameBeingSet: this.state.newNickname,
+            nicknameToSet: this.state.newNickname,
         });
         const message = {
             Type: "SetNickName",
@@ -89,37 +125,48 @@ class App extends Component {
 
     onResetNickname = () => {
         this.setState({
-            newNickname: this.state.currentNickname,
+            newNickname: this.state.nicknameToSet,
         });
     }
 
     render() {
+        const nicknames = this.state.nicknames.map((nickname) =>
+            <li key={nickname}>
+                {nickname}
+            </li>
+        );
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Chatty</h1>
                 </header>
                 <div className="App-main">
-                    <div>
-                        WebSocket status: <span className="App-Socket-status">{this.state.socketStatus}</span>
+                    <div className="App-main-left">
+                        <div>
+                            WebSocket status: <span className="App-Socket-status">{this.state.socketStatus}</span>
+                        </div>
+                        <div>
+                            <input
+                                value={this.state.serverUrl}
+                                onChange={this.changeServerUrl}
+                            />
+                            <button onClick={this.onConnect} >Connect</button>
+                            <button onClick={this.onDisconnect} >Disconnect</button>
+                        </div>
+                        <div>
+                            Nickname:
+                            <input
+                                className="App-Nickname-input"
+                                value={this.state.newNickname}
+                                onChange={this.changeNewNickname}
+                            />
+                            <button onClick={this.onSetNickname}>Set</button>
+                            <button onClick={this.onResetNickname}>Reset</button>
+                        </div>
                     </div>
-                    <div>
-                        <input
-                            value={this.state.serverUrl}
-                            onChange={this.changeServerUrl}
-                        />
-                        <button onClick={this.onConnect} >Connect</button>
-                        <button onClick={this.onDisconnect} >Disconnect</button>
-                    </div>
-                    <div>
-                        Nickname:
-                        <input
-                            className="App-Nickname-input"
-                            value={this.state.newNickname}
-                            onChange={this.changeNewNickname}
-                        />
-                        <button onClick={this.onSetNickname}>Set</button>
-                        <button onClick={this.onResetNickname}>Reset</button>
+                    <div className="App-main-right">
+                        Users currently here:
+                        <ul>{nicknames}</ul>
                     </div>
                 </div>
             </div>
