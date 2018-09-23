d312b3f0838cd27996ab8c5ed49851f1ca967fc0
Richard Walters <rwalters@digitalstirling.com>
2018-09-22 20:04:16 -0700

    Configure web server for secure (TLS) connections
    * Add self-signed server certificates/keys (two variations,
      one with a passphrase on the key, the other without).
    * Configure server to use secure connections, with
      certificate and key where key has a passphrase.
    * Stage the certificate and key where the key has a passphrase.

--- wstage.cmd
+++ wstage.cmd
@@ -1,8 +1,11 @@
 @set ROOT=%~dp0
-@set PLUGINS=%ROOT%build\WebServer\Debug\plugins
+@set STAGE=%ROOT%build\WebServer\Debug
+@set PLUGINS=%STAGE%\plugins
 @set RUNTIME=%PLUGINS%\runtime
 @if not exist %PLUGINS% mkdir %PLUGINS%
 @if not exist %RUNTIME% mkdir %RUNTIME%
 copy %ROOT%build\WebServer\ChatRoomPlugin\Debug\ChatRoomPlugin.dll %PLUGINS%
 copy %ROOT%build\WebServer\EchoPlugin\Debug\EchoPlugin.dll %PLUGINS%
 copy %ROOT%build\WebServer\StaticContentPlugin\Debug\StaticContentPlugin.dll %PLUGINS%
+copy %ROOT%test-cert-key-with-password\cert.pem %STAGE%
+copy %ROOT%test-cert-key-with-password\key.pem %STAGE%
