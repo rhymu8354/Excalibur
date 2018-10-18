f74dfb24c2bf24eadcce171309c3a258990fdd46
Richard Walters <rwalters@digitalstirling.com>
2018-10-18 14:37:29 -0700

    Change web server config to reflect personal preferences
    Use this commit if you call your build directory "build-vscode" like I do,
    and you use a CMake generator which is not multi-variant (I switched
    from MSBuild to Ninja).

--- wserve.cmd
+++ wserve.cmd
@@ -1,6 +1,6 @@
 @set ROOT=%~dp0
 @if "%~1"=="PePe" (
-    %ROOT%build\WebServer\Debug\WebServer -c %ROOT%config.json
+    %ROOT%build-vscode\WebServer\WebServer -c %ROOT%config.json
 ) else (
     call <nul %0 PePe
 )
--- wstage.cmd
+++ wstage.cmd
@@ -1,11 +1,11 @@
 @set ROOT=%~dp0
-@set STAGE=%ROOT%build\WebServer\Debug
+@set STAGE=%ROOT%build-vscode\WebServer
 @set PLUGINS=%STAGE%\plugins
 @set RUNTIME=%PLUGINS%\runtime
 @if not exist %PLUGINS% mkdir %PLUGINS%
 @if not exist %RUNTIME% mkdir %RUNTIME%
-copy %ROOT%build\WebServer\ChatRoomPlugin\Debug\ChatRoomPlugin.dll %PLUGINS%
-copy %ROOT%build\WebServer\EchoPlugin\Debug\EchoPlugin.dll %PLUGINS%
-copy %ROOT%build\WebServer\StaticContentPlugin\Debug\StaticContentPlugin.dll %PLUGINS%
+copy %ROOT%build-vscode\WebServer\ChatRoomPlugin\ChatRoomPlugin.dll %PLUGINS%
+copy %ROOT%build-vscode\WebServer\EchoPlugin\EchoPlugin.dll %PLUGINS%
+copy %ROOT%build-vscode\WebServer\StaticContentPlugin\StaticContentPlugin.dll %PLUGINS%
 copy %ROOT%test-cert-key-with-password\cert.pem %STAGE%
 copy %ROOT%test-cert-key-with-password\key.pem %STAGE%
