a7b373b7084447f6449c14c2902c1aab9705153a
Richard Walters <rwalters@digitalstirling.com>
2018-10-29 14:52:01 -0700

    Add another test certificate/key pair along with script/config
    This test certificate/key is useful for serving from "localhost",
    since it uses that common name.  Also included is a config file
    and script illustrating how the certificate/key pair is made.
    
    Change the wstage script to stage this certificate/key pair.

--- wstage.cmd
+++ wstage.cmd
@@ -7,5 +7,5 @@
 copy %ROOT%build-vscode\WebServer\ChatRoomPlugin\ChatRoomPlugin.dll %PLUGINS%
 copy %ROOT%build-vscode\WebServer\EchoPlugin\EchoPlugin.dll %PLUGINS%
 copy %ROOT%build-vscode\WebServer\StaticContentPlugin\StaticContentPlugin.dll %PLUGINS%
-copy %ROOT%test-cert-key-with-password\cert.pem %STAGE%
-copy %ROOT%test-cert-key-with-password\key.pem %STAGE%
+copy %ROOT%test-cert-key-localhost\cert.pem %STAGE%
+copy %ROOT%test-cert-key-localhost\key.pem %STAGE%
